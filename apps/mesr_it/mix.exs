d12ebd0d6c6bc3b188f80c4d37df8b66273ade6e
Alexander Korling <lermannen@gmail.com>
2018-01-02 21:25:27 +0100

    Update config

--- apps/mesr_it/mix.exs
+++ apps/mesr_it/mix.exs
@@ -4,7 +4,7 @@ defmodule MesrIt.Mixfile do
   def project do
     [
       app: :mesr_it,
-      version: append_revision("0.0.1"),
+      version: append_revision("0.0.2"),
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
